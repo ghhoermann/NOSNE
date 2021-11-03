103c4fa1b797e88bad5f90d237207b1c4143bc92
ghhoermann <93325377+ghhoermann@users.noreply.github.com>
2021-11-03 12:21:36 +0100

    Update WIn10Win10_Ent_21H2_en-us.ps1

--- WIn10Win10_Ent_21H2_en-us.ps1
+++ WIn10Win10_Ent_21H2_en-us.ps1
@@ -5,22 +5,7 @@ Write-Host  -ForegroundColor Cyan "Importing the OSD PowerShell Module"
 Import-Module OSD -Force
 
 #Start OSDCloud ZTI
-Write-Host  -ForegroundColor Cyan "Start Windows 10 Installation..."
-$Params = @{
-    OSBuild = "21H2"
-    OSEdition = "Enterprise"
-    OSLanguage = "en-us"
-    OSLicense = "Volume"
-    SkipAutopilot = $true
-    SkipAutopilotOOBE = $true
-    SkipODT = $true
-    ApplyCatalogFirmware = $false
-    ApplyManufacturerDrivers = $true
-    ApplyCatalogDrivers = $true
-    Screenshot = $true
-    ZTI = $true
-}
-Start-OSDCloud @Params
+Start-OSDCloud -SkipAutopilot -SkipODT -OSBuild 21H2 -OSEdition Enterprise -OSLanguage en-us -OSLicense Volume -ZTI
 
 #Restart from WinPE
 Write-Host  -ForegroundColor Cyan "Restarting in 20 seconds!"
